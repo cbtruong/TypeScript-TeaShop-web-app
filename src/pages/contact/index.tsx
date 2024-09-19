0ddaa853acfebaf5f1e96037225f675d66a5dc02
Dũng Nguyễn Quốc <nguyenquocdung26032003@gmail.com>
2024-09-19 21:39:48 +0700

    feat/update-contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -61,7 +61,7 @@ const Contact = () => {
                   required: "Email is required",
                   pattern: {
                     value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                    message: "Invalid email address",
+                    message: "Invalid email address.",
                   },
                 }}
                 render={({ field }) => (
